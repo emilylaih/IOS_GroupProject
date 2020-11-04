8dcac940875c91b7b9653139e42ca84acb5241a5
Min-yuan Lee <mina849669@gmail.com>
2020-11-04 15:01:51 -0800

    Remove I'm here folder

---
+++ I'm Here/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I'm Here
+//
+//  Created by Mina Lee on 11/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
