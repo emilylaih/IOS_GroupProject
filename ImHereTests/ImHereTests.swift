8985b164c02137a60128a5a4e953ecb74ba521f9
Min-yuan Lee <mina849669@gmail.com>
2020-11-06 01:44:53 -0800

    Rename ImHereTests and ImHereUITests

---
+++ ImHereTests/ImHereTests.swift
@@ -0,0 +1,33 @@
+//
+//  I_m_HereTests.swift
+//  I'm HereTests
+//
+//  Created by Mina Lee on 11/1/20.
+//
+
+import XCTest
+@testable import I_m_Here
+
+class I_m_HereTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
