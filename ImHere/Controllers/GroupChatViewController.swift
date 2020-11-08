0ec2f1807395e4308b01cae8d803a47b1ea32d7d
Min-yuan Lee <mina849669@gmail.com>
2020-11-08 06:10:14 -0800

    Add CreateNewGroup, GroupChat, GroupMap, Invitation, and GroupMembers viewController Scenes to main story broad and Files.

---
+++ ImHere/Controllers/GroupChatViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GroupChatViewController.swift
+//  ImHere
+//
+//  Created by Mina Lee on 11/8/20.
+//
+
+import UIKit
+
+class GroupChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
