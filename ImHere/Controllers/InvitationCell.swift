244c370dfbfe2e38c4760ebf37450c9b0c375144
Min-yuan Lee <mina849669@gmail.com>
2020-11-08 06:20:37 -0800

    Add LobbyGroupCell, MemberCell, and InvitationCell and connect them to individual tableViewCell with identifier.

---
+++ ImHere/Controllers/InvitationCell.swift
@@ -0,0 +1,23 @@
+//
+//  InvitationCell.swift
+//  ImHere
+//
+//  Created by Mina Lee on 11/8/20.
+//
+
+import UIKit
+
+class InvitationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ImHere/Controllers/LobbyGroupCell.swift
@@ -0,0 +1,23 @@
+//
+//  LobbyGroupCell.swift
+//  ImHere
+//
+//  Created by Mina Lee on 11/8/20.
+//
+
+import UIKit
+
+class LobbyGroupCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
